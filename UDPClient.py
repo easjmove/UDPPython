c61d6d3e2a1359895de7c69743ec1de677d03dd9
easjmove <90261592+easjmove@users.noreply.github.com>
2023-03-29 11:51:34 +0200

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -1,9 +1,8 @@
 from socket import *
 
-serverName = '255.255.255.255'
+serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 message = input('Input message:')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
